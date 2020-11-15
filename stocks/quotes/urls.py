f4ac1f1087c1a923c13d98aa92cfd413eed1ea8b
jashmakhija <jashmakhija06@gmail.com>
2020-11-16 00:18:11 +0530

    Table working

--- stocks/quotes/urls.py
+++ stocks/quotes/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/',views.about, name='about'),
     path('dashboard/',views.dashboard, name='dashboard'),
+    path('delete/<stock_id>',views.delete,name='delete'),
+    path('deletestock/',views.deletestock, name='deletestock'),
 ]
--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Stock
+from django.contrib import messages
+from .forms import StockForm
 
 # Create your views here.
 def home(request):
@@ -25,4 +28,38 @@ def about(request):
     return render(request, 'about.html',{})
 
 def dashboard(request):
-    return render(request, 'dashboard.html',{})
+    import requests
+    import json
+
+    if request.method == 'POST':
+        form =StockForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            messages.success(request,("Stock has been Added!"))
+            return redirect('dashboard')
+        
+    else:  
+        ticker =Stock.objects.all()
+        output =[]
+        for ticker_item in ticker:
+            api_request = requests.get("https://cloud.iexapis.com/stable/stock/" + str(ticker_item) + "/quote?token=pk_873df7d7e66c4c1fac2b36e03ab53d51")
+
+            try:
+                api =json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+        return render(request, 'dashboard.html',{'ticker':ticker,'output': output})
+
+
+
+def delete(request, stock_id):
+    item= Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request,("Stock has been deleted"))
+    return redirect(deletestock)
+
+def deletestock(request):
+    ticker = Stock.objects.all()
+    return render(request, 'deletestock.html',{'ticker': ticker})
